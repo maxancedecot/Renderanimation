694520b8882521b44f471bd8929df8ffd11a2240
maxancedecot <Dectomax@hotmail.fr>
2025-08-29 23:17:25 +0200

    chore(ui): use same favicon image as header logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 {/* Replace text logo with image logo */}
                 {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img
-                  src="https://pub-60f579eb256a4570ad9e0494f23007ac.r2.dev/logo%20ra.png"
+                  src="https://pub-60f579eb256a4570ad9e0494f23007ac.r2.dev/Favicon.png"
                   alt="RenderAnimation"
                   className="h-8 w-auto"
                 />
